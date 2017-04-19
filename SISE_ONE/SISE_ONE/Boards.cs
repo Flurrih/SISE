83fba75f24b8f682be93aa7a4c18505ec03e3b8b
Flurrih <ghost744@gmail.com>
2017-04-19 21:29:39 +0200

    Static Boards class with board samples

---
+++ SISE_ONE/SISE_ONE/Boards.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SISE_ONE
+{
+    static class Boards
+    {
+        public static int[,] targetBoard =
+        {
+                {1,2,3 },
+                {4,5,6 },
+                {7,8,0 }
+        };
+        public static int[,] puzzleShuffled =
+        {
+                {1,2,3 },
+                {4,6,8 },
+                {7,5,0 }
+        };
+        public static int[,] puzzleSolved =
+        {
+                {1,2,3 },
+                {4,5,6 },
+                {7,8,0 }
+        };
+    }
+}
